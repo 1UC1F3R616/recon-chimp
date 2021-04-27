3ad859cb20239d5cbfd840a80a78a9c2477c2189
1UC1F3R616 <kushchoudhary8@gmail.com>
2021-04-27 16:44:20 +0530

    crt_sh donw

--- scrapes/crt_sh.py
+++ scrapes/crt_sh.py
@@ -8,6 +8,10 @@ import requests
 from bs4 import BeautifulSoup
 
 def crt_sh(domain):
+    """
+    :input: domain name
+    :return-type: list
+    """
     query = 'https://crt.sh/?q={}'.format(domain)
 
     fetch = requests.get(query)
@@ -29,12 +33,33 @@ def crt_sh(domain):
     ############################
 
     soup = BeautifulSoup(fetch.text, 'html.parser')
-    subdomains = []
+    
+    subdomains = set()
+    
+
     tables = soup.find_all('table')
     table_with_subdomains = tables[1]
-    print(table_with_subdomains)
+    rows = table_with_subdomains.find_all('tr') # row[0] is somehow covering all rows, row[1] is not needed, start from row[2]
+    ## row[1] might not exist, case being handled, gives empty list ##
+    
+    for row in rows[2:]:
+        columns = row.find_all('td')
+    
+        total = []
+        for x in columns:
+            if (x.get('style')==None) and (x.find_all('a') == []):
+                total.append(x)
+        subdomains_only_column = total[-1]
+        for subdomain in subdomains_only_column:
+            if subdomain not in ['<br/>', '<br/>']:
+                subdomains.add(subdomain)
+    
+    subdomain_list = list(subdomains)
 
+    return subdomain_list
+
+            
 
 
 ## Testing Code
-crt_sh('vit.ac.in')
+print(crt_sh('dscvit2.com'))
