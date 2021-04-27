6c6dc9fabc27a1e36a320578280857d7343ef554
1UC1F3R616 <kushchoudhary8@gmail.com>
2021-04-28 01:23:30 +0530

    threatcrowd api done

---
+++ scrapes/threatcrowd.py
@@ -0,0 +1,27 @@
+############################################################################################
+# By: 1UC1F3R616
+# URL: https://www.threatcrowd.org/searchApi/v2/domain/report/?domain=
+# Date-Modified: 28-April-2020
+# Time avg: Time taken is under 30 seconds
+############################################################################################
+
+import requests
+
+def threatcrowd(domain):
+    """
+    :input: domain-name
+    :return: subdomains
+    :return-type: list
+    """
+    url = 'https://www.threatcrowd.org/searchApi/v2/domain/report/?domain={}'.format(domain)
+    fetch = requests.get(url)
+
+    subdomains = []
+
+    for subdomain in (fetch.json()).get('subdomains'):
+        subdomains.append(subdomain)
+
+    return subdomains
+
+## Test Code
+# print(threatcrowd('vit.ac.in'))
