c36d88719ef6d01f3ac0856ce4a56b94a361390d
BRO3886 <39856034+BRO3886@users.noreply.github.com>
2021-04-28 15:04:06 +0530

    feat(cli): add basic typer structure

--- cli/reconchimp.py
+++ cli/reconchimp.py
@@ -2,3 +2,26 @@ import typer
 from enum import Enum
 
 app = typer.Typer()
+
+
+class Notifier(Enum):
+    telegram = "telegram"
+    discord = "discord"
+
+
+@app.command()
+def do_recon(
+    domain: str = typer.Argument("", help="The domain name for the required host"),
+    all: bool = typer.Option(True, help="Selects all available tools for recon"),
+    notify: Notifier = typer.Option("", help="Select notification service"),
+):
+    """
+    performs reconnaissance on the supplied domain
+    """
+    # print(domain, notify.name)
+
+
+app()
+"""
+reconchimp dscvit.com --all --notify telegram|discord
+"""
