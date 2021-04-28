0d00c4e8a1aaf232442d815e698b5d86c01884b5
BRO3886 <39856034+BRO3886@users.noreply.github.com>
2021-04-28 10:01:24 +0530

    feat(notifier): add discord notifs

--- notifier/discord.py
+++ notifier/discord.py
@@ -3,21 +3,18 @@ import json
 from utils.config import config
 
 
-def send_tg_notif(chat: str = config("TELEGRAM_CHAT_ID")) -> bool:
+def send_discord_notif(url: str = config("DISCORD_WEBHOOK_URL")) -> bool:
     """
-    sends a telegram notification that work is completed
+    sends a notification via discord webhook
+    TODO: send analysis summary also (ref https://gist.github.com/Birdie0/78ee79402a4301b1faf412ab5f1cdcf9)
     """
-    bot_token = config("TELEGRAM_BOT_TOKEN")
-
-    url = f"https://api.telegram.org/bot${bot_token}/sendMessage"
     payload = json.dumps(
         {
-            "chat_id": chat,
-            "text": "workflow run completed.",
+            "content": "workflow run completed.",
         }
     )
     headers = {"Content-Type": "application/json"}
-    
+
     response = requests.request("POST", url, headers=headers, data=payload)
     if response.status_code == 200:
         return True
