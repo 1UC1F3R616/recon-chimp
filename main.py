fa6312806918cf4763e10c48da572fff20e18c40
1UC1F3R616 <kushchoudhary8@gmail.com>
2021-04-28 15:25:26 +0530

    http and ffuf added in pipeline using sh

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+## extrernal imports
+import urllib.request
+
+## internal imports
 from scrapes.c99 import c99
 from scrapes.crt_sh import crt_sh
 from scrapes.dnsdumpster import dnsdumpster
@@ -35,4 +39,9 @@ except Exception as e:
     print('[!] Failed to fetch Subdomains from threatcrowd')
     print('[!] {}'.format(str(e)))
 
-print(total_subdomains)
+# total_subdomains =list(total_subdomains)
+
+with open('temp_db/subdomains.txt', 'w') as f:
+    for subdomain in total_subdomains:
+        f.write(subdomain)
+        f.write('\n')
---
+++ rc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# python3 main.py
+# httpx -l temp_db/subdomains.txt -no-color -follow-host-redirects -status-code -title -content-length -web-server -silent >> temp_db/active_subdomains.txt
+
+# httpx -l temp_db/subdomains.txt -no-color -follow-host-redirects -silent >> temp_db/ffuf_subdomains.txt
+
+i=0
+while IFS= read -r line; do
+    i=$((i+1))
+    echo $i
+    filename="/home/kush/Desktop/github/recon-chimp/temp_db/ffuf_resolved/ffuf_resolved${i}.json"
+
+    ffuf -w dirlist.txt -u $line/FUZZ -o "${filename}"
+done < temp_db/ffuf_subdomains.txt
